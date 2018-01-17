7d84a498f5c287d5eb2c71f21af078961fe9b199
simonwinther <simon.winther@hotmail.com>
2018-01-17 14:56:21 +0100

    Initial Commit

---
+++ AppAboutSimonWinther/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AppAboutSimonWinther
+//
+//  Created by Simon Winther on 2018-01-17.
+//  Copyright © 2018 Simon Winther. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AppAboutSimonWinther/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AppAboutSimonWinther
+//
+//  Created by Simon Winther on 2018-01-17.
+//  Copyright © 2018 Simon Winther. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
